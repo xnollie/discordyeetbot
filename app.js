7ff5d3425e0e4a86f12fb3fe985d4bc5adc7beab
xnollie <33491412+xnollie@users.noreply.github.com>
2017-11-15 18:49:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./settings.json');
 
 client.on('ready',() => {
   console.log('I\'m Online');
