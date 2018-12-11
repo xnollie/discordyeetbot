b2e7eec986d9264fc4227597451ea00672b581e9
xnollie <33491412+xnollie@users.noreply.github.com>
2018-12-11 23:58:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ client.on('ready',() => {
 
 client.on("message", message => {
   if(message.author.bot) return;
+  
+  if(message.content.match(/bingo bango bongo/)){
+    message.channel.send("YEET! <@88328049401040896>"); // ping Hailey
+  }
 
   if(message.content.toLowerCase().match(/y[e3][e3]+t/)){
     message.channel.send("YEET "+message.guild.emojis.random());
