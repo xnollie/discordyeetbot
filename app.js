11a1ad0ff8f748a28d0919d3308e38868002c408
xnollie <33491412+xnollie@users.noreply.github.com>
2018-12-13 22:50:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ var yoteResponses = ["Say yote again motherfucker I dare you ...", "Umm 'scuse m
       message.channel.send(yoteResponses[Math.floor(Math.random() * (yoteResponses.length -1))])
   }
 
-  // 1 in 10.000 chance to ping user 
-  if(Math.floor(Math.random() * 10) === 37){
-    message.channel.send("YEET! <@131883237583618050>"); // ping Matthias for ballbusting purposes
+  // 1 in 1000 chance to ping user 
+  if(Math.floor(Math.random() * 1000) === 37){
+    message.channel.send("YEET! <@131883237583618050>"); // ping Mathias for ballbusting purposes
     //message.channel.send("YEET! <@283767301033951233>"); // ping Semanari for meme purposes
     //message.channel.send("YEET! <@88328049401040896>"); // ping Hailey for testing purposes
   }
