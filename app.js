f6251631834004da3555c5eb4d469c476cb5d20d
xnollie <33491412+xnollie@users.noreply.github.com>
2019-02-14 03:37:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on('ready',() => {
   console.log('I\'m Online');
 })
 
-var yoteResponses = ["Say yote again motherfucker I dare you ...", "Umm 'scuse me it's yeeted*", "The past tents of yeet is yeeted. Are you tupid or something?", "I shall yeet your yote and raise you a yeeted*"];
+var yoteResponses = ["Say yote again motherfucker I dare you ...", "<a:ANGRYAWOO:537664208863625272>", "Umm 'scuse me it's yeeted*", "The past tents of yeet is yeeted. Are you tupid or something?", "I shall yeet your yote and raise you a yeeted*"];
  
 // yeets in random text channel
 function randomYeet(){
